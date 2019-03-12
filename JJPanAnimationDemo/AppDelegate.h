38cfe032555feb19de61b380216bb062bad0baeb
cjj016 <cjj016@163.com>
2019-03-12 09:02:35 +0800

    Initial Commit

---
+++ JJPanAnimationDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  JJPanAnimationDemo
+//
+//  Created by Joey Chan on 2019/3/12.
+//  Copyright © 2019年 Joey Chan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ JJPanAnimationDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  JJPanAnimationDemo
+//
+//  Created by Joey Chan on 2019/3/12.
+//  Copyright © 2019年 Joey Chan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
